74cbd114500fc17b65a15bfae2bf5ca14b4cb456
Marco-MDV <marcodevincentiis71@gmail.com>
2024-01-24 21:04:22 +0100

    push M3 W1D1

---
+++ progetto13/S1P1/js/script.js
@@ -0,0 +1,30 @@
+/*  ESERCIZIO +1  */
+
+function removeTwitter(){
+    const ex1 = document.querySelector('aside div:nth-child(3) ol li:nth-child(2)')
+
+    ex1.innerHTML=' '
+}removeTwitter()
+
+/*  ESERCIZIO +2  */
+
+function removeContinuaALeggere(){
+    const father = document.querySelector('body>div>div>div p:last-of-type')
+    const jumbotron = document.querySelector('body>div>div>div p>a')
+
+    jumbotron.addEventListener('click',()=>{
+        father.innerHTML=' '
+    })
+}removeContinuaALeggere()
+
+/*  ESERCIZIO +3  */
+
+function hoverName(){
+    const names = document.querySelectorAll('main > div > div > div h2 + p a')
+   
+    names.forEach((element)=>{
+        element.addEventListener('mouseover',()=>{
+            alert(element.textContent)
+        })
+    })
+}hoverName()
