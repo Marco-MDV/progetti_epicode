4253f3ae43f7f3290e4085e1719ab2a1d751166e
Marco-MDV <marcodevincentiis71@gmail.com>
2024-03-07 12:38:01 +0100

    push last version

--- M4 W1D1/assests/js/script.js
+++ M4 W1D1/assests/js/script.js
@@ -15,7 +15,7 @@ function numberTwo(str, value) {
     } else {
         console.log("i can't");
     }
-} numberTwo()
+} numberTwo('ciao',1)
 
 
 function numberThree(num1, num2) {
@@ -43,7 +43,7 @@ function numberFive(array) {
         x += element
     });
     console.log(x);
-} numberFive(array = [1, 2, 3])
+} numberFive([1, 2, 3])
 
 
 function numberSix(array) {
@@ -52,7 +52,7 @@ function numberSix(array) {
     } else {
         console.log(true);
     }
-} numberSix(array = [2, 4, 8])
+} numberSix([2, 4, 8])
 
 
 function numberSeven(str, num) {
@@ -87,21 +87,139 @@ function numberEight(str) {
     let x = []
     if (z.some(space => space.trim() === "")) {
         console.log("you can't put only space, pls try again :)");
-    }else{
-        str.toLowerCase().split(" ").forEach(element=>{
-            x.push(element.slice(0,1))
+    } else {
+        str.toLowerCase().split(" ").forEach(element => {
+            x.push(element.slice(0, 1))
         })
         console.log(x.join('').toUpperCase());
     }
 } numberEight('Marco De Vincentiis')
 
-
 /* plus */
 
-function plusOne(str){
+function plusOne(str) {
+    let lettereArray = str.toLowerCase().split(' ').join('').split('')
 
-    const x = str.split(' ')
-    console.log(x);
-    
+    const recursività = {};
+    let elementRipetuto = []
+    let numRipetuto = 0
 
-}plusOne('io sono il carattere più usata')
+    lettereArray.map((lettera) => {
+        if (!recursività.hasOwnProperty(lettera)) {
+            recursività[lettera] = 0
+        }
+        recursività[lettera]++
+    })
+
+
+    for (const lettera in recursività) {
+        if (recursività[lettera] > numRipetuto) {
+            elementRipetuto.length = 0;
+            numRipetuto = recursività[lettera];
+        }
+        if (recursività[lettera] === numRipetuto) {
+            elementRipetuto.push(lettera);
+        }
+    }
+
+    console.log('elementi ripetuti sono:' + elementRipetuto);
+    console.log('si ripete:' + numRipetuto);
+} plusOne('proviamo con una "o" in più')
+
+const plusTwo = (str1,str2) =>{
+    let arraystr2=str2.toLowerCase().split(' ').join('').split('')
+    let count = 0;
+    /* console.log(arraystr2); */
+    arraystr2.forEach(element => {
+        /* console.log(element); */
+        const value = str1.search(element)
+        if(value >= 0){
+            count++
+        }else if(value < 0){
+            count--
+        }
+    });
+    if (count === str1.length) {
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+plusTwo('oaic','ciao')
+
+
+const plusThree = (str) =>{
+    return str.toLowerCase().split('').sort().join('')
+}
+
+const findAnagram = (str, possibleAnagram) =>{
+    const orderString = plusThree(str)
+    const correctAnagrams = possibleAnagram.filter((anagram)=>{
+        return plusThree(anagram) === orderString
+    })
+    return correctAnagrams
+}
+const word1='cane'
+const possibleAnagrams = [
+    'cane',
+    'cane!',
+    'enac'
+]
+
+console.log(findAnagram(word1,possibleAnagrams));
+
+const plusFour = (str) =>{
+    const palindrome = str.toLowerCase().split('').reverse().join('')
+    if(palindrome === str){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
+}
+plusFour('osso')
+
+const plusFive = (num) =>{
+    const palindromeNumber = String(num).split('').reverse().join('')
+    console.log(Number(palindromeNumber));
+}
+plusFive(28)
+
+const plusSix = (num) =>{
+    /* salve professore alla fine ho fatto il debug della scala questo è il mio risultato spero le piaccia */
+    for (let index = 0; index <= num; index++) {
+        if (index > 0) {
+            const mattone = document.createElement('p')
+            for (let index2 = 0; index2 < (index+1); index2++) {
+                const x = '#'.repeat(index2)
+                mattone.innerHTML=`<p>${x}</p>`
+            }
+            document.body.appendChild(mattone)            
+        }
+    }
+    /* questo era il metodo che mi fece vedere */
+    if (num<=0) {
+            console.log('not pos');
+        }
+        let htmlScale = ''
+        for (let i = 1; i <= num; i++) {
+            let scale = '#'.repeat(i)
+            console.log(scale);
+            htmlScale +=`<p>${scale}</p>`
+        }
+        const box = document.createElement('div')
+        box.innerHTML=htmlScale
+        document.body.appendChild(box)
+}
+plusSix(7)
+
+const plusSeven = (str) =>{
+    console.log(str.toLowerCase().split('').reverse().join(''));
+}
+plusSeven('ciao')
+
+const plusEight=(array,num)=>{
+    const size = Math.ceil(array.length/num);
+    console.log(Array.from({length:size},(v,i)=>array.slice(i * num,i*num+num)));
+}
+plusEight([1,2,3,4,5] , 4)
+
